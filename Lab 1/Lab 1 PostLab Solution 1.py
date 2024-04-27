762afeeb40e9502f2c890b22bd8cf05dfd8d3b29
ckmalipio <cyriskenmalipio@gmail.com>
2024-04-27 13:20:33 +0800

    Solution 1

--- Lab 1/Lab 1 PostLab Solution 1.py
+++ Lab 1/Lab 1 PostLab Solution 1.py
@@ -1,6 +1,21 @@
-'''Statisticians would like to have a set of functions to compute the median and mode of a list of numbers. 
-   The median is the number that would appear at the midpoint of a list if it were sorted. The mode is the 
-   number that appears most frequently in the list. Define these functions in a module named stats.py. Also 
-   include a function named mean, which computes the average of a set of numbers. Each function expects a list 
-   of numbers as an argument and returns a single number.'''
-   
+'''
+Statisticians would like to have a set of functions to compute the median and mode of a list of numbers. 
+The median is the number that would appear at the midpoint of a list if it were sorted. The mode is the 
+number that appears most frequently in the list. Define these functions in a module named stats.py. Also 
+include a function named mean, which computes the average of a set of numbers. Each function expects a list 
+of numbers as an argument and returns a single number.
+'''
+
+from stats import *
+
+def main():
+    input_str = input("Enter a list of numbers separated by spaces: ")
+    
+    nums = [int(num) for num in input_str.split()]
+    
+    print("Average:", getAverage(nums))
+    print("Median:", getMedian(nums))
+    print("Mode:", getMode(nums))
+
+if __name__ == "__main__":
+    main()
--- Lab 1/stats.py
+++ Lab 1/stats.py
@@ -8,15 +8,3 @@ def getMedian(nums):
 
 def getMode(nums):
     return statistics.mode(nums)
-
-def main():
-    input_str = input("Enter a list of numbers separated by spaces: ")
-    
-    nums = [int(num) for num in input_str.split()]
-    
-    print("Average:", getAverage(nums))
-    print("Median:", getMedian(nums))
-    print("Mode:", getMode(nums))
-
-if __name__ == "__main__":
-    main()
